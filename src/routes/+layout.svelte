8b91a5227e8ae473a6f39c425ca62f251153a515
Breno Brandão <brenoca35@gmail.com>
2024-08-21 14:09:32 -0300

    install tailwind correto + themes daisy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
+	import ThemeSwitch from '$lib/components/navbar/ThemeSwitch.svelte';
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+
+	onMount(() => {
+		themeChange(false);
+		// 👆 false parameter is required for svelte
+	});
 </script>
 
 <slot />
+<div class="mx-96">
+    <ThemeSwitch/>
+</div>
