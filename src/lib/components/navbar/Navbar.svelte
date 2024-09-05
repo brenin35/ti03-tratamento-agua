a907e3c79ff36814b5c964ee54c322992d9d0db4
Breno Brandão <brenoca35@gmail.com>
2024-09-05 10:42:17 -0300

    fonte + ajustes navbar

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -1,22 +1,20 @@
 <script>
-	import ThemeSwitch from "./ThemeSwitch.svelte";
-
+	import ThemeSwitch from './ThemeSwitch.svelte';
 </script>
-<nav class="bg-base-200  ">
+
+<nav class="bg-base-200">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-			<span class="self-center text-2xl font-semibold whitespace-nowrap "
-				>Flowbite</span
-			>
+			<img src="logo" class="h-8" alt="Aqua vida logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap">Aqua vida</span>
 		</a>
 		<div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <ThemeSwitch/>
+			<ThemeSwitch />
 
 			<button
 				data-collapse-toggle="navbar-user"
 				type="button"
-				class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden  focus:outline-none focus:ring-2 "
+				class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2"
 				aria-controls="navbar-user"
 				aria-expanded="false"
 			>
@@ -43,42 +41,15 @@
 			id="navbar-user"
 		>
 			<ul
-				class="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg bg-base-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-base-200  "
+				class="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg bg-base-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-base-200"
 			>
 				<li>
-					<a
-						href="#"
-						class="block py-2 px-3   rounded md:bg-transparent  md:p-0 "
-						aria-current="page">Home</a
+					<a href="/" class="block py-2 px-3 rounded md:bg-transparent md:p-0" aria-current="page"
+						>Página inicial</a
 					>
 				</li>
 				<li>
-					<a
-						href="#"
-						class="block py-2 px-3  rounded  md:hover:bg-transparent  md:p-0   "
-						>About</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 px-3  rounded  md:hover:bg-transparent  md:p-0   "
-						>Services</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 px-3  rounded  md:hover:bg-transparent  md:p-0   "
-						>Pricing</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 px-3  rounded  md:hover:bg-transparent  md:p-0   "
-						>Contact</a
-					>
+					<a href="/" class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0">Gerente</a>
 				</li>
 			</ul>
 		</div>
